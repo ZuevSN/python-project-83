be4fd1e82080375c29706cc2fefaa1e42e686c03
Sergei Zuev <zuevsn86@gmail.com>
2024-10-15 01:33:19 +0300

    fix

--- page_analyzer/additioanal_functions.py
+++ page_analyzer/additioanal_functions.py
@@ -26,14 +26,20 @@ def normalize(url):
 def get_html_data(url_id):
     description = None
     url = db.get_url_by_id(url_id)
+    print(url)
     response = requests.get(url['name'])
-    status_code = response.status_code
+#    status_code = response.status_code
+    response.raise_for_status()
+    print(status_code)
     soup = BeautifulSoup(response.text, 'html.parser')
     h1 = soup.find('h1')
     h1 = h1.text if h1 else None
+    print(h1)
     title = soup.find('title')
     title = title.text if title else None
+    print(title)
     meta_description = soup.find('meta', attrs={'name': 'description'})
     if meta_description:
         description = meta_description.get('content')
+        print(description)
     return (url_id, status_code, h1, title, description)
